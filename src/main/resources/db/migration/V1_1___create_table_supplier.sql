e768e67b22d7eb1f5393162b222e157592bd6515
Savina Ruxandra <savina_ruxandra@yahoo.com>
2021-07-20 14:08:45 +0300

    finished the data model

--- src/main/resources/db/migration/V1_1___create_table_supplier.sql
+++ src/main/resources/db/migration/V1_1___create_table_supplier.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `supplier` (
 
-    `id` int NOT NULL PRIMARY KEY,
+    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` varchar(20)
 
-)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+);
